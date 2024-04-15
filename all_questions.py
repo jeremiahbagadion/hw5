b1b022628190382cf3d1170d4ae7a0e7121c5169
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-04-15 18:11:14 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -176,7 +176,7 @@ def question8():
     answers = {}
 
     # type: eval_float
-    answers['(a) precision for C0'] = '0.1'
+    answers['(a) precision for C0'] = '(p * 100) / (p * 100 + p * 900)'
 
     # type: eval_float
     answers['(a) recall for C0'] = 'p'
